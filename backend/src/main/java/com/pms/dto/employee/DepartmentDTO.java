5a922c175ab988dea706e22c10e8ffed02b8650f
fredlin100 <118900490+fredlin100@users.noreply.github.com>
2026-05-24 18:03:09 +0800

    feat(employee): add common DTOs and properties

---
+++ backend/src/main/java/com/pms/dto/employee/DepartmentDTO.java
@@ -0,0 +1,15 @@
+package com.pms.dto.employee;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepartmentDTO {
+    private String departmentId;
+    private String name;
+}
